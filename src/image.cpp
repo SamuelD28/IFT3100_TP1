0260621a0910e2e3ab1e46ec2705b25c7a750ba6
samuel dubé <samuel_personnel@outlook.com>
2025-10-23 12:24:54 -0400

    wip: command parsing

--- src/image.cpp
+++ src/image.cpp
@@ -1,7 +1,5 @@
-
+#include <FreeImage.h>
 
 namespace image {
-
-	
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,66 @@
 #include <iostream>
-#include <shader.hpp>
-#include <application.hpp>
-#include <shape.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
 
+const char *COMMAND_SEPARATOR = ":";
+const char *ARGUMENT_SEPARATOR = ",";
+
+struct ConsoleCommand
+{
+	bool valid{};
+	std::string name{};
+	std::vector<std::string> arguments{};
+
+	void parse(std::string input)
+	{
+		auto separatorIndex = input.find(":");
+
+		if (separatorIndex == 0)
+		{
+			valid = false;
+		}
+
+		auto test = input.substr(0, separatorIndex - 1);
+		auto argumentsInput = input.substr(separatorIndex + 1, input.length());
+
+		std::cout << test << "\n";
+		std::cout << argumentsInput << "\n";
+
+		valid = true;
+	}
+};
+
+/**
+ * Console process to parse commands
+ *
+ * Commands
+ * quit : exit the current process
+ *
+ * Image part ---
+ * load : load a file at a given path, extract and save metadata
+ * Snap : Export current framebuffer for a given period
+ * palette : create a color palette that can be reused
+ *
+ */
 int main(int argc, char *argv[])
 {
-	auto window = application::setup("Labo 4", 800, 800);
-	if (window == nullptr)
+	// auto window = application::setup("Labo 4", 800, 800);
+	// if (window == nullptr)
+	// {
+	// 	std::cout << "There was an error initializing the window";
+	// 	std::cout.flush();
+	// 	return -1;
+	// }
+
+	auto command = ConsoleCommand{};
+
+	while (!command.valid)
 	{
-		std::cout << "There was an error initializing the window";
-		std::cout.flush();
-		return -1;
+		std::string input = "";
+		std::cin >> input;
+		command.parse(input);
 	}
 
-	auto vertices = std::vector<float>{0.0f, 0.0f, 0.5f, 0.5f};
-	unsigned int vbo, vao, ebo;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
-	glEnableVertexAttribArray(0);
-
-	// glGenBuffers(1, &ebo);
-	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
-
-	std::vector<shader::shader> shaders{
-			shader::shader{0, 0, "bin/shaders/line.vert", "", GL_VERTEX_SHADER},
-			shader::shader{0, 0, "bin/shaders/line.frag", "", GL_FRAGMENT_SHADER},
-	};
-
-	auto programId = shader::buildProgram(shaders);
-	glUseProgram(programId);
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBindVertexArray(vao);
-		glDrawArrays(GL_LINES, 0, vertices.size());
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
 	return 0;
 }
